7756c5d4b1727de088046dbb3140f454b9cf09fa
SOA-P <Amajoudnedim@gmail.com>
2022-10-31 19:29:05 +0100

    reprise sur la page project mise en place de cards

--- cv-react/src/Routes/Project.jsx
+++ cv-react/src/Routes/Project.jsx
@@ -4,12 +4,14 @@ import NavBarre from '../components/NavBarre'
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
 import PriceCard from '../components/PriceCard';
+import WorkCard from '../components/WorkCard';
 
 const Project = () => {
     return (
         <div>
             <NavBarre />
             <HeroImg2 heading="PROJECTS." text="Un clin d'oeil sur mes récents travaux personnels." />
+            <WorkCard/>
             <PriceCard/>
             <Footer />
         </div>
